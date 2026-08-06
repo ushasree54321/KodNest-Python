0fa1b1875fd7ca51f9def2ecfc7d3c385c69c715
usha sree <ushasree54321@gmail.com>
2026-08-06 12:10:48 +0530

    Day1changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/M02/T01/f string.py
@@ -0,0 +1,11 @@
+student_name=input()
+student_age=int(input())
+course_rating=float(input())
+
+print(f"Student: {student_name}")
+print(f"Age: {student_age}")
+print(f"Rating: {course_rating}")
+
+print(type(student_name))
+print(type(student_age))
+print(type(course_rating))
